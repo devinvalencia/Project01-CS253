7727e9ef4d8769a9e6e6adff207ba2a846954c19
devinvalencia <37150528+devinvalencia@users.noreply.github.com>
2018-09-21 22:53:13 -0400

    11PM - 9/21

--- src/Run.java
+++ src/Run.java
@@ -7,28 +7,28 @@ public class Run {
 
         while (selection != 7) {
             System.out.println("\nWhich program would you like to run?");
-            System.out.println("\n-------PART 1--------");
+            System.out.println("-------PART 1--------");
             System.out.println("[1] Bubble Sort \n[2] Insertion Sort \n[3] Selection Sort");
             System.out.println("\n-------PART 2--------");
             System.out.println("[4] Factorial \n[5] Fibonacci \n[6] Recursive Binary Search \n\n[7] Exit\n");
             selection = userinput.nextInt();
 
             if (selection == 1) {
-                /*
-                Bubble Sort
-                 */
+                int arr[] = {64, 34, 25, 12, 22, 11, 90};
+                BubbleSort.bubbleSort(arr);
+                BubbleSort.printArray(arr);
             }
 
             if (selection == 2) {
-                /*
-                Insertion Sort
-                 */
+                int arr[] = {64, 34, 25, 12, 22, 11, 90};
+                InsertionSort.sort(arr);
+                InsertionSort.printArray(arr);
             }
 
             if (selection == 3) {
-                /*
-                Selection Sort
-                 */
+                int arr[] = {64, 34, 25, 12, 22, 11, 90};
+                SelectionSort.sort(arr);
+                SelectionSort.printArray(arr);
             }
 
             if (selection == 4) {
@@ -46,7 +46,7 @@ public class Run {
             }
 
             if (selection == 6) {
-                int[] list = {9, 8, 1, 2, 6, 3, 4, 10, 5, 7};
+                int[] list = {1,2,3,4,5,6,7,8,9,10};
                 System.out.println("Please enter the number you are searching for (Between 1 & 10) ...");
                 int Ans = userinput.nextInt();
                 int index = RecursiveBinarySearch.recursiveBinarySearch(list,0,list.length,Ans);
